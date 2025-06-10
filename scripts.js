44ea658cb64cde59ab5c7254731bb73a5d3de259
carimcasta97 <carim.castaneda@navesoft.com>
2025-06-09 21:08:32 -0500

    ajustes segun

--- scripts.js
+++ scripts.js
@@ -4,16 +4,19 @@ const respuesta = document.getElementById('respuesta');
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const formData = new FormData(form);
+  const nombre = encodeURIComponent(form.nombre.value);
+  const correo = encodeURIComponent(form.correo.value);
+  const telefono = encodeURIComponent(form.telefono.value);
+  const fecha = encodeURIComponent(form.fecha.value);
+  const comentarios = encodeURIComponent(form.comentarios.value);
+
+  const url = `https://script.google.com/macros/s/AKfycbw2jiIsgxN6VhWctNInTfRp-tc52_I6_tZno3BqFhghwFJeod4UDegHwhZg76C_NdJF6Q/exec?nombre=${nombre}&correo=${correo}&telefono=${telefono}&fecha=${fecha}&comentarios=${comentarios}`;
 
   try {
-    const res = await fetch('https://script.google.com/macros/s/AKfycbw2jiIsgxN6VhWctNInTfRp-tc52_I6_tZno3BqFhghwFJeod4UDegHwhZg76C_NdJF6Q/exec', {
-      method: 'POST',
-      body: formData
-    });
-    console.log('res ='+ res);
-    
-    if (res.ok) {
+    const res = await fetch(url);
+    const result = await res.json();
+
+    if (result.status === 'ok') {
       respuesta.textContent = "¡Reserva enviada correctamente!";
       form.reset();
     } else {
